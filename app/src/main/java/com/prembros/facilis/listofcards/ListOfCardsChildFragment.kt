96c01b662d8c490b1801e764677f42f5cf754070
Prem Suman <prem@plank.life>
2018-12-27 15:40:46 +0530

    Added basic documentation

--- app/src/main/java/com/prembros/facilis/listofcards/ListOfCardsChildFragment.kt
+++ app/src/main/java/com/prembros/facilis/listofcards/ListOfCardsChildFragment.kt
@@ -30,9 +30,18 @@ class ListOfCardsChildFragment : BaseCardListChildFragment() {
         Picasso.get().load(imageUrl).into(thumbnail.dynamicHeightTarget(activity))
     }
 
+    /**
+     * Return the Drag area that should be used for swipe gestures, return null for no drag gesture implementations
+     */
     override fun getDragView(): View? = dragArea
 
+    /**
+     * Return the root view (preferably a CardView) of the fragment
+     */
     override fun getRootView(): ViewGroup? = rootCard
 
+    /**
+     * Return the Drag handle resource ID to toggle visibility during transition
+     */
     override fun dragHandleId(): Int = R.id.drag_handle_image
 }
--- app/src/main/java/com/prembros/facilis/listofcards/ListOfCardsContainerFragment.kt
+++ app/src/main/java/com/prembros/facilis/listofcards/ListOfCardsContainerFragment.kt
@@ -13,11 +13,23 @@ class ListOfCardsContainerFragment : BaseCardListContainerFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
             inflater.inflate(R.layout.fragment_list_of_cards_container, container, false)
 
+    /**
+     * Return the number of items to be inflated in the [viewPager]
+     */
     override fun baseCardCount(): Int = 10
 
+    /**
+     * Return the background layout (preferably BlurLayout) for effects during transition, return null for transparent background and no effects
+     */
     override fun backgroundBlurLayout(): ViewGroup? = blurLayout
 
+    /**
+     * Return the [ViewPager] which will contain the [BaseCardFragment]s
+     */
     override fun viewPager(): ViewPager = cardViewPager
 
+    /**
+     * Put here what you'd put in the [getItem(position: Int)] method of the [ViewPager]
+     */
     override fun baseCardToInflate(position: Int): BaseCardFragment = ListOfCardsChildFragment.newInstance(getRandomImageUrl())
 }
